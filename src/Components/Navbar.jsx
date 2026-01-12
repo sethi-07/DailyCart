6ec251d636d2686847e4637b879fff23a7ed6cbb
Abhinandan <abhinandansethi07@gmail.com>
2026-01-12 17:14:46 +0530

    Mobile menu added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
-import { Link, NavLink} from 'react-router'
+import React, { useState } from 'react'
+import { Link, NavLink} from 'react-router-dom'
 import { SiDailydotdev } from "react-icons/si";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+
 const navLinks = [
   {
     page:'home',
@@ -20,18 +23,30 @@ const navLinks = [
   }
 ]
 const Navbar = () => {
-  const NavItems = ({item}) => {
-    return <NavLink to = {item?.link}>{item?.page}</NavLink>
-  }
+
+  const [isOpenMenu, setOpenMenu] = useState(false);
+
+  const NavItems = ({ item, className = '' }) => {
   return (
-    <div className='w-full h-15 bg-blue-300'>
-      <div className='mx-auto px-4 pt-4 w-full flex items-center justify-around gap-10 capitalize'>
+    <NavLink
+      to={item?.link}
+      className={`hover:text-blue-900 transition-colors ${className}`}
+    >
+      {item?.page}
+    </NavLink>
+  )
+}
+
+  return (
+    <div className='w-full h-16 bg-black text-white'>
+      <div className='mx-auto px-6 h-full w-full flex items-center justify-between  gap-10 capitalize'>
      
-      <div>
-          <Link to = {"/login"} className='text-base flex items-center gap-2 font-medium'><SiDailydotdev />DailyCart</Link>
+      <div className='flex gap-6'>
+          <button onClick={() => setOpenMenu(!isOpenMenu)} className='cursor-pointer text-3xl md:hidden'><GiHamburgerMenu /></button>
+          <Link to = {"/login"} className='text-lg flex items-center gap-2 text-cyan-200 font-medium'><SiDailydotdev />DailyCart</Link>
       </div>
 
-      <nav className='flex gap-5'>
+      <nav className='hidden md:flex gap-6 font-semibold'>
         {
           navLinks?.map(item =>(
             <NavItems item={item} />
@@ -39,11 +54,30 @@ const Navbar = () => {
         }
       </nav>
 
-      <div className='flex items-center gap-2'>
+      <div className='flex items-center gap-3'>
         <Link to = {'/login'} className='text-base capitalize font-semibold'>login</Link>
         <span className='h-5 w-0.5 bg-[#ff9d00] inline-block rounded-2xl'></span>
         <Link to = {'/register'} className='text-base capitalize font-semibold'>signup</Link>
+        <Link to = {'/'} className='w-10 h-10' ><img src="/rm.jpg" className='w-full h-full object-cover rounded-full border-white border-2'></img></Link>
       </div>
+
+      <nav
+      className={`fixed top-16 left-0 max-w-[350px] w-full h-screen
+      bg-cyan-900 text-white z-50
+      transition-transform duration-200
+      ${isOpenMenu ? 'translate-x-0' : '-translate-x-full'}
+      flex flex-col justify-center p-10 gap-10 text-3xl
+      md:-translate-x-full`}
+      >
+      {navLinks.map(item => (
+        <NavItems
+          key={item.page}
+          item={item}
+          className="hover:text-blue-900"
+        />
+      ))}
+    </nav>
+
     </div>
     </div>
   )
